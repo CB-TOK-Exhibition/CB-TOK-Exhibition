98c2f8e851dd0ae9723ef5d9a0bca81bccac63d9
Edwin <jessicawu414@gmail.com>
2021-05-03 20:12:49 -0400

    types thing

--- src/types/projects.ts
+++ src/types/projects.ts
@@ -1,6 +1,7 @@
 export default interface project{
-    project_name: string,
-    topics: Number[]
+    projectTitle: string,
+    topics: Number[],
+    class: string,
 }
 
 export type topics = "Knowledge and the Knower" | "Knowledge and Language" | "Knowledge and Tech" | "Knowledge and Religion" | "Knowledge and Politics" | "Knowledge and Indigenous Societies"
